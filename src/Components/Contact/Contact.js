09277f5134815e4106493e8daf38fefb1e92fd4a
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-05-10 05:32:54 +0600

    emailjs integrated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,44 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import emailjs from 'emailjs-com';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 import './Contact.css';
 
 const Contact = () => {
+    const [contactModalShow, setContactModalShow] = React.useState(false);
+
+    function MyVerticallyCenteredModal2(props) {
+        return (
+          <Modal
+            {...props}
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Body>
+                <div className="text-center">
+                    <h5>Thanks for your submission.</h5>
+                    <div className="pt-2">
+                        <Button onClick={props.onHide}>Close</Button>
+                    </div>
+                </div>
+            </Modal.Body>
+          </Modal>
+        );
+      }
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_q3keunh', 'template_gd3iv7y', e.target, 'user_XdzmVkjI8eROeWQNbDQJa')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+    }
+
     return (
         <>
             <section className="contact">
@@ -15,17 +51,18 @@ const Contact = () => {
                                         available at your fingertips?
                                         <br /><span>send me a, hey.</span>
                                     </p>
-                                    <form action="" method="post">
+                                    <form onSubmit={sendEmail}>
                                         <div className="information-name">
-                                            <input id="name" type="text" name="query" placeholder="Your Query"/>
+                                            <input id="name" type="text" name="message" placeholder="Your Query"/>
                                         </div>
 
                                         <div className="information-email">
-                                            <input id="name" type="email" name="query" placeholder="Your Email"/>
+                                            <input id="name" type="email" name="reply_to" placeholder="Your Email"/>
                                         </div>
 
                                         <div className="information-submit">
-                                            <input type="submit" name="SUBMIT" value="Send"/>
+                                            <input type="submit" name="SUBMIT" value="Send" onClick={() => setContactModalShow(true)}/>
+                                            <MyVerticallyCenteredModal2 show={contactModalShow} onHide={() => setContactModalShow(false)}/>
                                         </div>
                                     </form>
                                     <p>
